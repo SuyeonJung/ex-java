ecd34ac00c123696b1a8d78030d9bdb991720422
SuyeonJung <suuu0126@naver.com>
2019-02-25 16:46:46 +0900

    first example

---
+++ src/Ex04.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Ex04 {
+  public static void main(String[] args) {
+
+	  
+	  int n = 0 ; // 삼각형 가로의 길이 
+	  System.out.println("삼각형의 층 수 를 입력해주세요 :");
+	  Scanner sc = new Scanner(System.in);
+	  
+	  n = Integer.parseInt(sc.nextLine());
+	  n = (n*2) -1; //층수를 (2*n-1)로 해서 가로의 크기로 바꾼다 
+	  
+	  System.out.println();
+	  
+	  //삼각형
+	  
+	  for(int i = 1; i<=n ; i+=2) {
+		  //빈칸 찍는 반복문 
+		  for(int k = 1; k<=(n-i)/2;k++) {
+			  System.out.print(" ");
+		  }
+		  for(int j=1; j<=i;j++) {
+			  System.out.print("*");
+		  }
+		  System.out.println();
+	  }
+	  System.out.println("삼각형 완성!");
+  }
+}
