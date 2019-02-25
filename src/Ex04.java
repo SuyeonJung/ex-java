9fa7befd2659fe2c8cedbcfad5ad4fbb98cf7d93
SuyeonJung <suuu0126@naver.com>
2019-02-25 18:37:03 +0900

    inverted triangle print

--- src/Ex04.java
+++ src/Ex04.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 class Ex04 {
   public static void main(String[] args) {
-
-	  
+ 
 	  int n = 0 ; // 삼각형 가로의 길이 
 	  System.out.println("삼각형의 층 수 를 입력해주세요 :");
 	  Scanner sc = new Scanner(System.in);
 	  
 	  n = Integer.parseInt(sc.nextLine());
-	  n = (n*2) -1; //층수를 (2*n-1)로 해서 가로의 크기로 바꾼다 
+	 /* n = (n*2) -1; //층수를 (2*n-1)로 해서 가로의 크기로 바꾼다 
 	  
 	  System.out.println();
 	  
@@ -26,5 +25,18 @@ class Ex04 {
 		  System.out.println();
 	  }
 	  System.out.println("삼각형 완성!");
+	  */
+	  
+	 for(int i = n; i>0;i--) {
+		 for(int j=i-1;j<n;j++) {
+			 System.out.print(" ");
+		 }
+		 for(int k=i*2-1;k>0;k--) {
+			 System.out.print("*");
+		 }
+		 System.out.println();
+	 }
+	  System.out.println("역삼각형 완성!");
+	 
   }
 }
